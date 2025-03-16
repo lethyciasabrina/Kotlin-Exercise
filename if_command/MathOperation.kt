0a535c8c98723a8a5fe131237b4a6ce43cff59f0
lethyciasabrina <lethyciasabrinaleal@gmail.com>
2025-03-16 16:16:24 -0300

    Refactor MathOperation

--- if_command/MathOperation.kt
+++ if_command/MathOperation.kt
@@ -5,21 +5,22 @@ fun main() {
     val number1 = readln().toInt()
     println("Enter another number: ")
     val number2 = readln().toInt()
-    println("Choose an operation: 1) +, 2) -, 3) *, 4) /")
-    val choose = readln()
+    println("Choose an operation: +, -, * or /")
+    val operation = readln()
 
-    val plus = number1 + number2
-    val minus = number1 - number2
-    val multipliedBy = number1 * number2
-    val dividedBy = number1 / number2
-
-    if (choose == "1") {
-        println("$number1 + $number2 = $plus")
-    } else if (choose == "2") {
-        println("$number1 - $number2 = $minus")
-    } else if (choose == "3") {
-        println("$number1 * $number2 = $multipliedBy")
-    } else if (choose == "4") {
-        println("$number1 / $number2 = $dividedBy")
+    if (operation == "+") {
+        println("$number1 + $number2 = ${number1 + number2}")
+    } else if (operation == "-") {
+        println("$number1 - $number2 = ${number1 - number2}")
+    } else if (operation == "*") {
+        println("$number1 * $number2 = ${number1 * number2}")
+    } else if (operation == "/") {
+        if (number2 == 0) {
+            println("Invalid Operation! Division by 0")
+        } else {
+            println("$number1 / $number2 = ${number1 / number2}")
+        }
+    } else {
+        println("Invalid Operation.")
     }
 }
