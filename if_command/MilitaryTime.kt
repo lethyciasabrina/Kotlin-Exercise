b46557fddeb539606309790b7116744a484b9f2b
lethyciasabrina <lethyciasabrinaleal@gmail.com>
2025-03-18 20:08:52 -0300

    Refactor MilitaryTime.kt

--- if_command/MilitaryTime.kt
+++ if_command/MilitaryTime.kt
@@ -1,22 +1,26 @@
 package if_command
 
 fun main() {
-    println("Enter the current hour (military time 0 to 23): ")
-    val militaryTime = readln().toInt()
+    println("Enter the hour (military time 0 to 23): ")
+    val hour = readln().toInt()
 
-    if (militaryTime == 0) {
-        println("Midnight")
-    } else if (militaryTime >= 1 && militaryTime <= 11) {
-        println("Morning")
-    } else if (militaryTime == 12) {
-        println("Noon")
-    } else if (militaryTime >= 13 && militaryTime <= 17) {
-        println("Afternoon")
-    } else if (militaryTime >= 18 && militaryTime <= 20) {
-        println("Evening")
-    } else if (militaryTime >= 21 && militaryTime <= 23) {
-        println("Night")
-    } else {
+    if (hour < 0 || hour > 23) {
         println("Invalid Operation!")
+    } else {
+        if (hour <= 11) {
+            if (hour == 0) {
+                println("Midnight")
+            } else {
+                println("Morning")
+            }
+        } else if (hour <= 17) {
+            if (hour == 12) {
+                println("Noon")
+            } else {
+                println("Afternoon")
+            }
+        } else {
+            println("Night")
+        }
     }
 }
