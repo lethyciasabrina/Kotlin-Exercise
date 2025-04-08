b9c661b323797ff40ff5b16131d83745666ccdd6
Lethycia Sabrina <lethyciasabrinaleal@gmail.com>
2025-04-07 22:17:57 -0300

    Refactor CalculateTotalPrice

--- if_command/CalculateTotalPrice.kt
+++ if_command/CalculateTotalPrice.kt
@@ -2,21 +2,23 @@ package if_command
 
 fun main() {
     println("Type the quantity of a product: ")
-    val quantity = readln().toDouble()
+    val quantity = readln().toDoubleOrNull()
 
-    if (quantity < 0) {
+    if (quantity == null || quantity < 0) {
         println("Invalid operation!")
     } else {
+        var unitPrice = 0.0
         if (quantity < 5) {
-            println("Total price: ${quantity * 1.50}")
+            unitPrice = 1.50
         } else if (quantity < 10) {
-            println("Total price: ${quantity * 1.45}")
+            unitPrice = 1.45
         } else if (quantity < 50) {
-            println("Total price: ${quantity * 1.40}")
+            unitPrice = 1.40
         } else if (quantity < 100) {
-            println("Total price: ${quantity * 1.30}")
+            unitPrice = 1.30
         } else {
-            println("Total price: ${quantity * 1.20}")
+            unitPrice = 1.20
         }
+        println("Total price: ${unitPrice * quantity}")
     }
 }
