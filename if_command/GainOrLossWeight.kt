a0e7390d084c244cc0ced3c7ad1e1af597a255a6
Lethycia Sabrina <lethyciasabrinaleal@gmail.com>
2025-04-24 20:58:10 -0300

    Refactor GainOrLossWeight

--- if_command/GainOrLossWeight.kt
+++ if_command/GainOrLossWeight.kt
@@ -10,23 +10,27 @@ fun main() {
     println("Enter your gender: ")
     val gender = readln().uppercase()
 
-    val idealWeight: Double
-    if (gender == "F") {
-        idealWeight = (62.1 * height) - 44.7
-    } else if (gender == "M") {
-        idealWeight = (72.7 * height) - 58
+    if (height < 0 || height > 2.5 || gender.length != 1 || !gender[0].isLetter()) {
+        println("Invalid Operation")
     } else {
-        println("I don't have a formula for the given gender.")
-        return
-    }
-    println("Hi $name, your ideal weight is ${"%.2f".format(idealWeight)}kg.")
-    if (currentWeight > idealWeight) {
-        val loseWeight = currentWeight - idealWeight
-        println("You're overweight and need to lose ${"%.2f".format(loseWeight)}kg.")
-    } else if (currentWeight < idealWeight) {
-        val gainWeight = idealWeight - currentWeight
-        println("You're underweight and need to gain ${"%.2f".format(gainWeight)}kg.")
-    } else {
-        println("You look great, congrats! :) ")
+        val idealWeight: Double
+        if (gender == "F") {
+            idealWeight = (62.1 * height) - 44.7
+        } else if (gender == "M") {
+            idealWeight = (72.7 * height) - 58
+        } else {
+            println("I don't have a formula for the given gender.")
+            return
+        }
+        println("Hi $name, your ideal weight is ${"%.2f".format(idealWeight)}kg.")
+        if (currentWeight > idealWeight) {
+            val loseWeight = currentWeight - idealWeight
+            println("You're overweight and need to lose ${"%.2f".format(loseWeight)}kg.")
+        } else if (currentWeight < idealWeight) {
+            val gainWeight = idealWeight - currentWeight
+            println("You're underweight and need to gain ${"%.2f".format(gainWeight)}kg.")
+        } else {
+            println("You look great, congrats! :) ")
+        }
     }
 }
