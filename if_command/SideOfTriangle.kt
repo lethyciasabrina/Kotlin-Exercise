6687542990a53f7d766f417d7d82b2861b19418d
Lethycia Sabrina <lethyciasabrinaleal@gmail.com>
2025-04-29 19:19:23 -0300

    Refactor SideOfTriangle

--- if_command/SideOfTriangle.kt
+++ if_command/SideOfTriangle.kt
@@ -10,10 +10,12 @@ fun main() {
 
     if (side1 == null || side1 < 0 || side2 == null || side2 < 0 || side3 == null || side3 < 0) {
         println("Invalid Operation.")
+    } else if (side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1) {
+        println("It is not a triangle.")
+    } else if (side1 == side2 && side1 == side3) {
+        println("Equilateral")
     } else if (side1 != side2 && side2 != side3 && side3 != side1) {
         println("Scalene")
-    } else if (side1 == side2 && side3 == side1) {
-        println("Equilateral")
     } else {
         println("Isosceles")
     }
