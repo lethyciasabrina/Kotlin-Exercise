841eadda3578aff8add960f273f3c1eded534398
Lethycia Sabrina <lethyciasabrinaleal@gmail.com>
2025-04-29 21:47:35 -0300

    Refactor NewSalary

--- if_command/NewSalary.kt
+++ if_command/NewSalary.kt
@@ -7,17 +7,16 @@ fun main() {
     val salary = readln().toDouble()
 
     val raising: Int
-    if (salary <= 50000) {
+    if (salary in 0.0..50000.0) {
         raising = 20
-    } else if (salary > 50001 && salary < 100000) {
+    } else if (salary in 50001.0..100000.0) {
         raising = 15
-    } else if (salary > 100001 && salary < 150000) {
+    } else if (salary in 100001.0..150000.0) {
         raising = 10
     } else {
         raising = 5
     }
-    val salaryIncrease = (raising / 100.0) * salary
-    val newSalary = salaryIncrease + salary
+    val newSalary = salary + salary * (raising / 100.0)
     println(
         "Hi $name your raise is ${raising}% and your new salary is ${"%.2f".format(newSalary)}"
     )
