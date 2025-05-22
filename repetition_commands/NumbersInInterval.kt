a1882c005a8bf0d442aab7c72b24813c4189ebc7
Lethycia Sabrina <lethyciasabrinaleal@gmail.com>
2025-05-21 22:43:23 -0300

    Refactor NumbersInInterval

--- repetition_commands/NumbersInInterval.kt
+++ repetition_commands/NumbersInInterval.kt
@@ -2,16 +2,16 @@ package repetition_commands
 
 fun main() {
     println("Type a number: ")
-    val number1 = readln().toInt()
+    val min = readln().toInt()
     println("Type another number: ")
-    val number2 = readln().toInt()
-    val start = minOf(number1, number2)
-    val end = maxOf(number1, number2)
-    val range = start..end
-    for (numbers in range) {
-        if (numbers == start || numbers == end) {
-            continue
+    val max = readln().toInt()
+    if (min < max) {
+        for (numbers in min..max) {
+            print("$numbers ")
         }
-        print("$numbers ")
+    } else {
+        for (numbers in min downTo max) {
+            print("$numbers ")
+        }
     }
 }
