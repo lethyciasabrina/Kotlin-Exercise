bfc78ac9dd113b84264584ef67bba291a36ff097
Lethycia Sabrina <lethyciasabrinaleal@gmail.com>
2025-05-30 21:52:40 -0300

    feat: handling null cases

--- repetition_commands/TwoHighestUnique.kt
+++ repetition_commands/TwoHighestUnique.kt
@@ -1,17 +1,19 @@
 package repetition_commands
 
 fun main() {
-    var max1 = 0
-    var max2 = 0
+    var max1: Double? = null
+    var max2: Double? = null
     do {
         println("Numbers: ")
         val input = readln()
         if (input.isNotEmpty()) {
-            val number = input.toInt()
-            if (number > max1) {
+            val number = input.toDouble()
+            if (max1 == null) {
+                max1 = number
+            } else if (number > max1) {
                 max2 = max1
                 max1 = number
-            } else if (number > max2) {
+            } else if (max2 == null || number > max2) {
                 max2 = number
             }
         }
