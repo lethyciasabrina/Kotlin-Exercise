2e79d0a9e941d2636f9e79fc99618c77e0d93e7e
Lethycia Sabrina <lethyciasabrinaleal@gmail.com>
2025-08-14 21:46:05 -0300

    Renaming variables

--- repetition_commands/InteractiveMenu.kt
+++ repetition_commands/InteractiveMenu.kt
@@ -1,36 +1,36 @@
 package repetition_commands
 
 fun main() {
-    var pedido = -1
+    var order = -1
     var total = 0.0
     while (true) {
         println(
-            "Escolha uma opção:\n" +
+            "Choose an option:\n" +
                     "1 - Pizza (R$25.00)\n" +
-                    "2 - Hambúrguer (R$18.00)\n" +
-                    "3 - Refrigerante (R$5.00)\n" +
-                    "4 - Suco (R$7.00)\n" +
-                    "0 - Sair"
+                    "2 - Hamburger (R$18.00)\n" +
+                    "3 - Soda (R$5.00)\n" +
+                    "4 - Juice (R$7.00)\n" +
+                    "0 - Exit"
         )
-        pedido = readln().toInt()
-        when (pedido) {
+        order = readln().toInt()
+        when (order) {
             1 -> total += 25.0
             2 -> total += 18.0
             3 -> total += 5.0
             4 -> total += 7.0
             0 -> {
-                println("Total final: R$$total")
-                println("Volte sempre! :) ")
+                println("Final Total: R$$total")
+                println("Come back soon! :) ")
                 break
             }
 
-            else -> println("Opção inválida")
+            else -> println("Invalid option.")
         }
-        println("Total Parcial: R$$total")
-        println("Deseja continuar? (s/n)")
+        println("Partial Total: R$$total")
+        println("Do you want to continue? (y/n)")
         if (readln().lowercase() == "n") {
-            println("Total final: R$$total")
-            println("Volte sempre! :)")
+            println("Final total: R$$total")
+            println("Come back soon! :) ")
             break
         }
     }
