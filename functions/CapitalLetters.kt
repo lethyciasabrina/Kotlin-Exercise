4ee46b39e4171b7db677d599a86659c5d5231eda
Lethycia Sabrina <lethyciasabrinaleal@gmail.com>
2025-08-25 22:28:02 -0300

    solved CapitalLetters

---
+++ functions/CapitalLetters.kt
@@ -0,0 +1,15 @@
+package functions
+
+fun main() {
+    val names = mutableListOf<String>()
+    println("Add names (type 'exit' to finish): ")
+    while (true) {
+        val inputUser = readln()
+        if (inputUser.equals("exit", ignoreCase = true)) {
+            break
+        } else {
+            names.add(inputUser)
+        }
+    }
+    println("Names in uppercase: ${names.map { it.uppercase() }}")
+}
