8f418f93e644fdf788a088c0a6c9bc5d5fd3c938
Lethycia Sabrina <lethyciasabrinaleal@gmail.com>
2025-09-01 22:42:38 -0300

    solved Task

--- functions/Task.kt
+++ functions/Task.kt
@@ -1,5 +1,18 @@
 package functions
 
 fun main() {
-
+    val task = mutableListOf<String>()
+    var inputTask: String
+    while (true) {
+        print("Enter a task (type 'exit' to finish): ")
+        inputTask = readln()
+        if (inputTask.equals("exit", ignoreCase = true)) {
+            break
+        } else {
+            task.add(inputTask)
+        }
+    }
+    task.forEach {
+        println("Task: $it")
+    }
 }
