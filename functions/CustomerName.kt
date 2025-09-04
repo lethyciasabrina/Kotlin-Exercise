774004e5a5892171f75ba3fb864610f466068d35
Lethycia Sabrina <lethyciasabrinaleal@gmail.com>
2025-09-03 22:32:34 -0300

    refactoring CustomerName

--- functions/CustomerName.kt
+++ functions/CustomerName.kt
@@ -3,18 +3,10 @@ package functions
 fun main() {
     print("Name: ")
     val name: String? = readln()
-    val userName: Customer? = searchName(name)
-    val customerName = userName?.name ?: "Name not found"
-    println("Customer: $customerName")
-}
-
-fun searchName(name: String?): Customer? {
-    return if (name.isNullOrBlank()) {
-        null
-    } else {
-        Customer(name)
+    name?.let {
+        val customer = Customer(name = it)
+        println("Customer: ${customer.name}")
     }
 }
 
-data class Customer(val name: String?)
-
+data class Customer(val name: String = "")
