e9261ea18ee5d013557b414a7c56b2f468ce5891
Lethycia Sabrina <lethyciasabrinaleal@gmail.com>
2025-09-04 21:34:35 -0300

    refactoring CalculatingAndFormatting

--- functions/CalculatingAndFormatting.kt
+++ functions/CalculatingAndFormatting.kt
@@ -3,11 +3,11 @@ package functions
 data class Price(var price: Double)
 
 fun main() {
-    print("Coin: ")
+    print("Price: ")
     val inputPrice = readln().toDouble()
-    val price = Price(inputPrice).run {
-        val discount = price - (price * 0.1)
+    val priceWithDiscount = Price(inputPrice).run {
+        val discount = this.price - (this.price * 0.1)
         "The final price with 10% discount is: R$ ${String.format("R$%.2f", discount)}"
     }
-    println(price)
+    println(priceWithDiscount)
 }
