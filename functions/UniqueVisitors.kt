93c2802465dd6476f317167cc5fe23043bc47b5b
Lethycia Sabrina <lethyciasabrinaleal@gmail.com>
2025-09-21 16:41:36 -0300

    solved UniqueVisitors

---
+++ functions/UniqueVisitors.kt
@@ -0,0 +1,15 @@
+package functions
+
+fun main() {
+    val visitorsName = mutableSetOf<String>()
+    while (true) {
+        println("Enter the visitors name (type 'done' to exit): ")
+        val name = readln()
+        if (name.equals("done", ignoreCase = true)) {
+            break
+        } else {
+            visitorsName.add(name)
+        }
+    }
+    println("Total unique visitors: ${visitorsName.size}\nNames: $visitorsName")
+}
