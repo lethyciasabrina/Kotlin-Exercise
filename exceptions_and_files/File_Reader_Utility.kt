3cf1f716e38154d4e25fd3c4e406c76ecc113884
Lethycia Sabrina <lethyciasabrinaleal@gmail.com>
2025-10-27 11:38:18 -0300

    add File_Reader_Utility question

---
+++ exceptions_and_files/File_Reader_Utility.kt
@@ -0,0 +1,28 @@
+package exceptions_and_files
+
+import java.io.File
+import java.io.IOException
+
+fun main() {
+    val filename = "file_reader.txt"
+    val lines = readLogLines(filename)
+    if (lines != null) {
+        for (line in lines) {
+            println(line)
+        }
+    }
+}
+
+fun readLogLines(filename: String): List<String>? {
+    val file = File(filename)
+    if (!file.exists()) {
+        print("ERROR: File not found")
+        return null
+    }
+    try {
+        return file.readLines()
+    } catch (e: IOException) {
+        print("ERROR: I/O error reading file")
+        return null
+    }
+}
